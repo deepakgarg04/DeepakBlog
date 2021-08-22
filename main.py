5323784d9078b7301d5da49255a85cdd3f585373
Deepak <deepak.garg@abacusinsights.com>
2021-08-22 19:55:32 +0530

    Chmaged the size of email data type

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class BlogPost(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     children = db.relationship("BlogPost")
